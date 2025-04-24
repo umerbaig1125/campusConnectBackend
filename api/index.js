f1cd115982e134bc5c934a5e210158497b303a1f
umerbaig1125 <42769031+umerbaig1125@users.noreply.github.com>
2025-04-24 19:51:01 +0500

    Updated backend structure for Vercel deployment

--- api/index.js
+++ api/index.js
@@ -1,2 +1,36 @@
-const app = require('../server');
+// const app = require('../server');
+// module.exports = app;
+
+const express = require('express');
+const connectDB = require('../db');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const authRoutes = require('../routes/auth');
+const eventRoutes = require('../routes/events');
+const feedbackRoutes = require('../routes/feedback');
+const voteRoutes = require('../routes/vote');
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Log all incoming requests (debugging)
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
+
+// DB connect
+connectDB();
+
+// Routes
+app.use('/api/auth', authRoutes);
+app.use('/api/events', eventRoutes);
+app.use('/uploads', express.static('uploads'));
+app.use('/api/feedback', feedbackRoutes);
+app.use('/api/vote', voteRoutes);
+
+// Export the app to be used by Vercel serverless functions
 module.exports = app;
